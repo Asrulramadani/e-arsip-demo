565f5ef62b13164d385a02ddcfa894b8018bc051
Asrul Ramadani <68553467+Asrulramadani@users.noreply.github.com>
2022-09-10 20:16:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ require("./app/routes")(app);
 // db.user.create(user);
 
 // running app on port based on .env file
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 9001
 
 app.listen(PORT, () => {
   console.log(`🚀Server running at : http://localhost:${PORT} 🚀`);
